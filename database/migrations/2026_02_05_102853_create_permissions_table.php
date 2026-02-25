1b47dfd2d3e3986118fdc1f9f3b87523f70d5eb8
khamdullaevuz <elbek.khamdullaev@gmail.com>
2026-02-25 18:02:13 +0500

    remove menu type

--- database/migrations/2026_02_05_102853_create_permissions_table.php
+++ database/migrations/2026_02_05_102853_create_permissions_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('type');
             $table->string('name');
             $table->timestamps();
         });
